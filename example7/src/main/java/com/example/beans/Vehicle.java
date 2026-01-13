531a1911ba0b38c7c2297e04750880ca8ac44095
lav-k <lavkush.pnbe@gmail.com>
2026-01-13 23:32:17 +0530

    Programmatically adding beans to the spring context

---
+++ example7/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,20 @@
+package com.example.beans;
+
+public class Vehicle {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello()
+    {
+        System.out.println("Printing hello from component vehicle bean");
+    }
+
+    private String name;
+
+}
